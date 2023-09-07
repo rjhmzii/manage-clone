85f5d65f6f45b39fb522a7074d316566d76403b0
Hamza Abid <rjhmzii@outlook.com>
2023-09-07 18:43:04 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '440px',
+      md: '768px',
+      lg: '970px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
